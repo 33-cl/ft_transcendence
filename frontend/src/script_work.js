e84880a335e53924eeb91e013db5506d523d53c3
Rayane Samad <rasamad@z2r8p2.42lyon.fr>
2025-06-02 13:00:22 +0200

    check collision

--- frontend/src/script_work.js
+++ frontend/src/script_work.js
@@ -9,15 +9,15 @@ const paddleWidth = 10;
 const paddleCollisionSurface = paddleMargin + paddleWidth;
 const paddleHeight = 110;
 let leftPaddleY = rightPaddleY = canvas.height / 2 - paddleHeight / 2; //position du padd
-let paddleSpeed = 15
+let paddleSpeed = 10
 
 //Balle
 let ballPaused = false; // Indique si la balle est en pause
 let ballX = canvas.width / 2;
 let ballY = canvas.height / 2;
 let ballRadius = 20;
-let ballSpeedX = 7;
-let ballSpeedY = 7;
+let ballSpeedX = 8;
+let ballSpeedY = 8;
 
 //Score
 let leftScore;
