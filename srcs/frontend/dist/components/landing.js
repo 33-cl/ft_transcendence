c5c943a660884096b86956f21883ef8636c671f0
qordoux <quentin.odx@gmail.com>
2025-06-12 19:05:22 +0200

    improved coms

--- srcs/frontend/dist/components/landing.js
+++
@@ -1,7 +0,0 @@
-export const landingHTML = /*html*/ `
-    <div id="landing-buttons">
-        <button id="signInBtn">Sign in</button>
-        <button id="guestBtn">Continue as guest</button>
-    </div>
-`;
-//# sourceMappingURL=landing.js.map
--- srcs/frontend/dist/components/signIn.js
+++
@@ -1,9 +0,0 @@
-export const signInHTML = /*html*/ `
-    <h2>Sign in</h2>
-    <input type="text" id="username" placeholder="Username">
-    <input type="password" id="password" placeholder="Password">
-    <button id="signIn">SIGN IN</button>
-
-    <p>Don't have an account? <a id="signUpBtn" class="link">Sign up</a></p>
-`;
-//# sourceMappingURL=signIn.js.map
