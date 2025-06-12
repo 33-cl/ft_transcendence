8d5a95c360060b34955460f03a5ec0c0e3bed1b8
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-06-12 18:22:22 +0200

    html in different files

---
+++ srcs/frontend/src/components/mainMenu.ts
@@ -0,0 +1,25 @@
+export const mainMenuHTML = /*html*/`
+    <div class="menu-container">
+        <div class="menu-section">
+            <h2>Play</h2>
+            <div class="button-group">
+                <button>1v1 Ranked</button>
+                <button id="localGameBtn">Local</button>
+            </div>
+        </div>
+        <div class="menu-section">
+            <h2>Custom</h2>
+            <div class="button-group">
+                <button>Create</button>
+                <button>Join</button>
+            </div>
+        </div>
+        <div class="menu-section">
+            <h2>Tournaments</h2>
+            <div class="button-group">
+                <button>Create</button>
+                <button>Join</button>
+            </div>
+        </div>
+    </div>
+`; 
