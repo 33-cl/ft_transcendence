2cd84f79c47cf9dcb0c96c78bde9d75cd3929d83
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-06-15 01:00:50 +0200

    Fix button hitbox bug

--- srcs/frontend/src/components/game.ts
+++ srcs/frontend/src/components/game.ts
@@ -1,6 +1,6 @@
 export const gameHTML = /*html*/`
     <canvas id="map" width="850px" height="650px"></canvas>
-    <div id="score">
+    <div id="score" class="text-2xl">
         <span id="leftScore">0</span> -  <span id="rightScore">0</span>
     </div>
     <p id="winnerDisplay"></p>
