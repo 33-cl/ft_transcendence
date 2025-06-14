cd7bb2199f9b086e9d3d3fd9f6d4934de06faf81
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-06-14 23:57:34 +0200

    Style with Tailwind CSS

--- srcs/frontend/tailwind.config.js
+++ srcs/frontend/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
+    "./**/*.html",
+    "./*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'press-start': ['"Press Start 2P"', 'cursive'],
+      },
+    },
   },
+  safelist: [
+    'font-press-start'
+  ],
   plugins: [],
 } 
