7d57fd7bd742ba6fc0d7f24bb6e82326d2065c79
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-06-17 00:34:52 +0200

    friend list front

---
+++ srcs/frontend/src/components/friendList.ts
@@ -0,0 +1,14 @@
+export const friendListHTML = /*html*/`
+    <div class="friendList">
+        <h2>Friends</h2>
+        <hr>
+        <div class="friend">
+            <img src="./img/tung-tung-tung-sahur.jpeg" alt="Friend Avatar">
+            <p class="friend-name">Tung Tung Tung Sahur</p>
+        </div>
+        <div class="friend">
+            <img src="./img/brr-brr-patapim.jpg" alt="Friend Avatar">
+            <p class="friend-name">Brr Brr Patapim</p>
+        </div>
+    </div>
+`;
--- srcs/frontend/src/components/index.ts
+++ srcs/frontend/src/components/index.ts
@@ -1,5 +1,6 @@
 export { landingHTML } from './landing.js';
 export { signInHTML } from './signIn.js';
 export { signUpHTML } from './signUp.js';
+export { friendListHTML } from './friendList.js';
 export { mainMenuHTML } from './mainMenu.js';
 export { gameHTML } from './game.js'; 
