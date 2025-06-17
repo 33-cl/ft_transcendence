29dd627377f33fa9ef045dd12a993e6de6fe5ba5
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-06-17 16:55:05 +0200

    stars in the background

--- srcs/frontend/src/components/friendList.ts
+++ srcs/frontend/src/components/friendList.ts
@@ -10,5 +10,21 @@ export const friendListHTML = /*html*/`
             <img src="./img/brr-brr-patapim.jpg" alt="Friend Avatar">
             <p class="friend-name">Brr Brr Patapim</p>
         </div>
+        <div class="friend">
+            <img src="./img/tung-tung-tung-sahur.jpeg" alt="Friend Avatar">
+            <p class="friend-name">Tung Tung Tung Sahur</p>
+        </div>
+        <div class="friend">
+            <img src="./img/brr-brr-patapim.jpg" alt="Friend Avatar">
+            <p class="friend-name">Brr Brr Patapim</p>
+        </div>
+        <div class="friend">
+            <img src="./img/tung-tung-tung-sahur.jpeg" alt="Friend Avatar">
+            <p class="friend-name">Tung Tung Tung Sahur</p>
+        </div>
+        <div class="friend">
+            <img src="./img/brr-brr-patapim.jpg" alt="Friend Avatar">
+            <p class="friend-name">Brr Brr Patapim</p>
+        </div>
     </div>
 `;
