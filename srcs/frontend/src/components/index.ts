bc52f79b327a8312477c2567233eb32380881d9e
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-06-17 20:01:35 +0200

    Add leaderboard + crowns on pfps

--- srcs/frontend/src/components/index.ts
+++ srcs/frontend/src/components/index.ts
@@ -1,6 +1,7 @@
 export { landingHTML } from './landing.js';
 export { signInHTML } from './signIn.js';
 export { signUpHTML } from './signUp.js';
+export { leaderboardHTML } from './leaderboard.js';
 export { friendListHTML } from './friendList.js';
 export { mainMenuHTML } from './mainMenu.js';
-export { gameHTML } from './game.js'; 
+export { gameHTML } from './game.js';
