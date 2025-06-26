d2e68d58b3a1d4e73006b3bd70c7768024ced621
qordoux <quentin.odx@gmail.com>
2025-06-26 18:11:55 +0200

    yes

--- srcs/docker/frontend/entrypoint.sh
+++ srcs/docker/frontend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# Génère un certificat auto-signé si besoin
+# Génère un certificat auto-signé pour nginx
 if [ ! -f /etc/nginx/ssl/nginx.key ] || [ ! -f /etc/nginx/ssl/nginx.crt ]; then
   mkdir -p /etc/nginx/ssl
   openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
