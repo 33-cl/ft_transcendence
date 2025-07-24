f29b72e0b5e435aced09cf270e767bfaa3fc4ba1
Rayane <rayane.tisec77@gmail.com>
2025-07-24 13:18:17 +0200

    ball score left (ballX - ballRadius) pour que le point soit comtabiliser quand la balle touche le bord

--- srcs/backend/Rayan/pong.ts
+++ srcs/backend/Rayan/pong.ts
@@ -203,7 +203,7 @@ export class PongGame {
                 this.state.ballX = paddles[1].x - ballRadius;
             }
             // But à gauche
-            if (this.state.ballX < 0) {
+            if (this.state.ballX - this.state.ballRadius < 0) {
                 paddles[1].score++;
                 console.log(`[BACKEND] But ! Score mis à jour - Gauche: ${paddles[0].score}, Droite: ${paddles[1].score}`);
                 this.resetBall();
