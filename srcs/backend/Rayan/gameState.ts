688a7fa436ef070b9b3fe5893140f8617bd9c6b7
Rayane <rayane.tisec77@gmail.com>
2025-08-19 17:39:16 +0200

    modification des cacluls de collision

--- srcs/backend/Rayan/gameState.ts
+++ srcs/backend/Rayan/gameState.ts
@@ -94,8 +94,8 @@ export function createInitialGameState(numPlayers: number = 2): GameState {
         ballX:          canvasWidth / 2,
         ballY:          canvasHeight / 2,
         ballRadius:     20,
-        ballSpeedX:     3,
-        ballSpeedY:     3,
+        ballSpeedX:     1,
+        ballSpeedY:     1,
         win:            4,
         running:        false,
         ballCountdown:  3, // Délai de 3 secondes avant que la balle commence
