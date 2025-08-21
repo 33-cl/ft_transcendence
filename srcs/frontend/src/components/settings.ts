da70876e22a6c978b35865c3afd913d9843c7655
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-08-21 13:51:07 +0200

    more options on settings

--- srcs/frontend/src/components/settings.ts
+++ srcs/frontend/src/components/settings.ts
@@ -1,9 +1,13 @@
 export function settingsHTML() {
     const username = window.currentUser?.username || 'user666';
+    const email = window.currentUser?.email || 'unknown@gmail.com';
+
     return /*html*/ `
     <div id="settings-form">
         <h1>USER SETTINGS</h1>
         <p>USERNAME     ${username}</p>
+        <p>AVATAR       [Change]</p>
+        <p>EMAIL        ${email}</p>
         
 
         <button id="goToMain">[BACK]</button>
