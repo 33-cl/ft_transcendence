e7b222ed85c07f9b594f79d90b594dbd669ec495
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-08-21 15:49:16 +0200

    style on setting page

--- srcs/frontend/src/components/settings.ts
+++ srcs/frontend/src/components/settings.ts
@@ -1,14 +1,14 @@
 export function settingsHTML() {
     const username = window.currentUser?.username || 'user666';
     const email = window.currentUser?.email || 'unknown@gmail.com';
-    
+
 
     return /*html*/ `
     <div id="settings-form">
         <h1>USER SETTINGS</h1>
-        <p>USERNAME&emsp;&emsp;&emsp;${username}</p>
-        <p>AVATAR&emsp;&emsp;&emsp;&emsp;<span id="change-pp">[Change]</span></p>
-        <p>EMAIL&emsp;&emsp;&emsp;&emsp;&emsp;${email}</p>
+        <p>USERNAME&emsp;&emsp;&emsp;<input type="text" id="username" placeholder=${username}></p>
+        <p>AVATAR&emsp;&emsp;&emsp;&emsp;&emsp;<span id="change-pp">[Change]</span></p>
+        <p>EMAIL&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<input type="text" id="email" placeholder=${email}></p>
         
 
         <div id="settings-buttons">
