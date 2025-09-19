84963bc05df450d7c3b64e089993b1ee7573ebed
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-09-20 00:09:44 +0200

    crown on friendlist

--- srcs/frontend/src/components/friendList.html.ts
+++ srcs/frontend/src/components/friendList.html.ts
@@ -1,27 +1,45 @@
 export async function friendListHTML() {
     try {
-        const response = await fetch('/users', {
+        // Récupérer les utilisateurs récents
+        const usersResponse = await fetch('/users', {
             method: 'GET',
             credentials: 'include'
         });
         
-        if (!response.ok) {
+        if (!usersResponse.ok) {
             throw new Error('Failed to fetch users');
         }
         
-        const data = await response.json();
-        const users = data.users || [];
+        const usersData = await usersResponse.json();
+        const users = usersData.users || [];
+
+        // Récupérer le leaderboard pour identifier le premier
+        const leaderboardResponse = await fetch('/users/leaderboard?limit=1', {
+            method: 'GET',
+            credentials: 'include'
+        });
+
+        let firstRankUserId = null;
+        if (leaderboardResponse.ok) {
+            const leaderboardData = await leaderboardResponse.json();
+            if (leaderboardData.leaderboard && leaderboardData.leaderboard.length > 0) {
+                firstRankUserId = leaderboardData.leaderboard[0].id;
+            }
+        }
         
         let userItems = '';
         
         users.forEach((user: any) => {
             const avatarUrl = user.avatar_url || './img/default-pp.jpg';
+            const isFirstRank = user.id === firstRankUserId;
+            const crownIcon = isFirstRank ? '<img src="./img/gold-crown.png" alt="First place" class="crown-icon crown" style="position: absolute; top: -5px; right: -5px; width: 20px; height: 20px; z-index: 10;">' : '';
             
             userItems += `
-                <div id="profileBtn" class="friend" data-username="${user.username}">
+                <div id="profileBtn" class="friend" data-username="${user.username}" style="position: relative;">
                     <img src="${avatarUrl}" alt="${user.username} Avatar" class="profile-pic" 
                          onerror="this.onerror=null;this.src='./img/default-pp.jpg';">
                     <p class="friend-name">${user.username}</p>
+                    ${crownIcon}
                 </div>
             `;
         });
