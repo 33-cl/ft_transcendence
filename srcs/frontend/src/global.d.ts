0131f95d72da20c2a0ebec2c2b9f059b8ed30f68
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-10-21 09:55:00 +0200

    fix: user now cannot change his email if oauth user

--- srcs/frontend/src/global.d.ts
+++ srcs/frontend/src/global.d.ts
@@ -24,11 +24,12 @@ declare global
 			email: string;
 			username: string;
 			avatar_url: string | null;
-			wins?: number;
-			losses?: number;
-			created_at: string;
-			updated_at: string;
-		} | null;
+		wins?: number;
+		losses?: number;
+		created_at: string;
+		updated_at: string;
+		provider?: string;
+	} | null;
 		logout?: () => Promise<void>;
 	}
 }
