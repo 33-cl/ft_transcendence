bdd38e63d9f424b8dc5b8df0824c6cc7b684119d
Rayane-Hub <rayane.tisec77@gmail.com>
2025-10-22 17:55:58 +0200

    chore(frontend): Update 4-player game canvas size
    - Update canvas width from 850 to 1200
    - Update canvas height from 650 to 800
    - Ensure consistency across all game modes

--- srcs/frontend/src/components/game4.html.ts
+++ srcs/frontend/src/components/game4.html.ts
@@ -1,4 +1,4 @@
 export const game4HTML = /*html*/`
-    <canvas id="map" width="850px" height="650px"></canvas>
+    <canvas id="map" width="1200px" height="800px"></canvas>
     <div id="score" class="text-2xl"></div>
 `;
