74ac57717ea09ce1fedcc5b08d74c7ea2cc5a917
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-10-23 15:26:32 +0200

    init: box created with text inside

--- srcs/frontend/src/components/mainMenu.html.ts
+++ srcs/frontend/src/components/mainMenu.html.ts
@@ -1,5 +1,13 @@
 export const mainMenuHTML = /*html*/`
     <div class="menu-container">
+        <div class="menu-section" id="joinNextGame">
+            <div id="textInfos">
+                <div id="Join-next-Game">Join next Game</div>
+                <div id="gameMode">4 players</div>
+                <div id="nbPlayers">1/4</div>
+                <div id="time">42s</div>
+            </div>
+        </div>
         <div class="menu-section">
             <h1><span class="clickable">Local</span></h1>
             <div class="button-group">
@@ -11,9 +19,10 @@ export const mainMenuHTML = /*html*/`
             <h1><span class="clickable">Multiplayer</span></h1>
             <div class="button-group">
                 <button id="ranked1v1Btn" class="default-button">2 Player</button>
-                <button id="customJoinBtn" class="default-button">3 Player</button>
+                <button id="customJoinBtn" class="default-button">4 Player</button>
             </div>
         </div>
+        <!--
         <div class="menu-section">
             <h1><span class="clickable">Tournaments</span></h1>
             <div class="button-group">
@@ -21,6 +30,7 @@ export const mainMenuHTML = /*html*/`
                 <button id="tournamentJoinBtn" class="default-button">Battle Royale</button>
             </div>
         </div>
+        -->
     </div>
 `;
 
