205c43e13a43c4609e019fd4afe5071335b7bd0d
Rayane-Hub <rayane.tisec77@gmail.com>
2025-10-26 15:41:30 +0100

    rename button 3player ->  4player

--- srcs/frontend/src/components/mainMenu.html.ts
+++ srcs/frontend/src/components/mainMenu.html.ts
@@ -11,7 +11,7 @@ export const mainMenuHTML = /*html*/`
             <h1><span class="clickable">Multiplayer</span></h1>
             <div class="button-group">
                 <button id="ranked1v1Btn" class="default-button">2 Player</button>
-                <button id="customJoinBtn" class="default-button">3 Player</button>
+                <button id="multiplayer4pBtn" class="default-button">4 Player</button>
             </div>
         </div>
         <div class="menu-section">
