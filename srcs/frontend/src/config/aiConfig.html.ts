4bc99bb705eb12d5f6236410e931817495f761a8
33-cl <1.2.3.4.5.6.33cl@gmail.com>
2025-11-05 18:21:40 +0100

    update: change icons for ai config

--- srcs/frontend/src/config/aiConfig.html.ts
+++ srcs/frontend/src/config/aiConfig.html.ts
@@ -8,12 +8,16 @@ export const aiConfigHTML = /*html*/`
             <h2>Choose game mode:</h2>
             <div class="game-mode-options">
                 <button id="vs-ai" class="game-config-button">
-                    <span class="difficulty-icon">🤖</span>
+                    <span class="difficulty-icon">
+                        <img src="./img/bot.svg" alt="Bot icon" class="game-mode-icon"/>
+                    </span>
                     <span class="difficulty-name">VS AI</span>
                     <span class="difficulty-desc">Fight the machine</span>
                 </button>
                 <button id="vs-player" class="game-config-button">
-                    <span class="difficulty-icon">👥</span>
+                    <span class="difficulty-icon">
+                        <img src="./img/two-players.svg" alt="Two players icon" class="game-mode-icon"/>
+                    </span>
                     <span class="difficulty-name">2 PLAYERS</span>
                     <span class="difficulty-desc" id="local2p">Local multiplayer</span>
                 </button>
